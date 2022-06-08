e2f01c48ecd0cb252d1a33a361f8459bf442ef7a
Interlinked <israel.rocha.rocha@live.com>
2022-06-07 22:36:20 -0400

    bootstrap & db config

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -36,8 +36,8 @@
 <script>
     function selectParamCell(clicked_id) {
         document.getElementById(clicked_id).innerHTML = clicked_id;
-        let $popid = "pop" + clicked_id;
-        document.getElementById($popid).innerHTML = $popid;
+//        let $popid = "pop" + clicked_id;
+//       document.getElementById($popid).innerHTML = $popid;
     }
 
 </script>
@@ -54,32 +54,4 @@ const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl, /*True in order to edit popover html*/{html: true}));
 </script>
 
-<!--
-<script>
-    function selectParamCell(clicked_id) {
-        document.getElementById(clicked_id).innerHTML = clicked_id;
-    }
-</script>
-
-
-<br>
-<br>
-<button id="1" onClick="reply_click(this.id)">B1</button>
-<button id="2" onClick="reply_click(this.id)">B2</button>
-<button id="3" onClick="reply_click(this.id)">B3</button>
-<p id="response"></p>
-    
-<script type="text/javascript">
-  function reply_click(clicked_id)
-  {
-      document.getElementById("response").innerHTML = clicked_id;
-  }
-</script>
-
-<script>
-    function paramInsert() {
-        document.getElementById("param{{ $product->id }}").innerHTML = "Hello World";
-    }
-</script>
--->
 @endsection
